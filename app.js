69aa0c036cc882bfc73a82739a19bdac4b28c88a
Agush <89817777+Agustin-Clemente@users.noreply.github.com>
2023-01-29 17:52:10 -0300

    Modo oscuro implementado y nuevos estilos

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const contenedorQR= document.getElementById('contenedorQR');
 const formulario = document.getElementById('formulario');
 const QR= new QRCode(contenedorQR);
+const botonSwitch= document.querySelector('#switch');
+
 
 formulario.addEventListener('submit', (event)=>{
     event.preventDefault();
     QR.makeCode(formulario.url.value);
 });
 
+function irALaPagina(){
+    window.open(formulario.url.value,'_blank')
+}
 
+
+botonSwitch.addEventListener('click', ()=> {
+    document.body.classList.toggle('dark');
+    botonSwitch.classList.toggle('active');
+
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true');
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
+})
+
+if(localStorage.getItem('dark-mode')=== 'true'){
+    document.body.classList.add('dark');
+    botonSwitch.classList.add('active');
+}else{
+    document.body.classList.remove('dark');
+    botonSwitch.classList.remove('active');
+}
 
 
 //new QRCode(contenedorQR, 'https://miportfolio-b96db.web.app/portfolio');
